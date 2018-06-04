61ef672baf2e50a6e4a404e3b768d14676cd5c92
Gaby G <paogaby0803@gmail.com>
2018-06-04 16:05:16 -0500

    mandala geometrica

---
+++ Mandala.py
@@ -0,0 +1,183 @@
+import turtle
+
+class GeometryMandala:
+    turtle.shape("turtle")
+
+    '''turtle.left(45)
+    turtle.color("blue")
+    turtle.fill(True)
+    # first squera
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.fill(False)
+    turtle.fillcolor("violet")
+
+
+    turtle.right(45)
+    turtle.forward(25)
+
+
+    turtle.left(135)
+    turtle.forward(85)
+    turtle.left(90)
+    turtle.forward(85)
+    turtle.left(90)
+    turtle.forward(85)
+    turtle.left(90)
+    turtle.forward(85)
+
+
+    turtle.color("red")
+
+    turtle.left(45)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(60)
+
+    turtle.right(90)
+    turtle.forward(20)
+
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(80)
+
+    turtle.color("purple")
+
+    turtle.right(90)
+    turtle.forward(34)
+    turtle.left(90)
+
+    turtle.circle(114)
+
+    turtle.color("grey")
+    turtle.left(45)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)'''
+
+    turtle.speed("fastest")
+    turtle.color("violet")
+    turtle.fill(True)
+    turtle.circle(142)
+    turtle.fill(False)
+
+    turtle.color("purple")
+    turtle.fill(True)
+    turtle.left(45)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.fill(False)
+    turtle.home()
+    turtle.left(90)
+
+
+    turtle.forward(142)
+    turtle.left(45)
+    turtle.forward(142)
+
+    turtle.color("pink")
+    turtle.fill(True)
+    turtle.right(135)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(135)
+
+    turtle.forward(30)
+
+    turtle.fill(False)
+
+    turtle.color("red")
+    turtle.right(45)
+    turtle.fill(True)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.left(90)
+    turtle.forward(160)
+    turtle.fill(False)
+
+    turtle.left(90)
+    turtle.forward(80)
+
+    turtle.color("yellow")
+    turtle.fill(True)
+    turtle.left(45)
+    turtle.forward(114)
+    turtle.left(90)
+    turtle.forward(114)
+    turtle.left(90)
+    turtle.forward(114)
+    turtle.left(90)
+    turtle.forward(114)
+
+    turtle.left(90)
+    turtle.forward(57)
+
+    turtle.fill(False)
+
+    turtle.color("purple")
+    turtle.fill(True)
+    turtle.left(45)
+    turtle.forward(80)
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.fill(False)
+
+    turtle.color("green")
+    for i in range(1,40):
+        turtle.left(90)
+        turtle.forward(80-i*2)
+        turtle.left(90)
+        turtle.forward(80-i*2)
+        turtle.left(90)
+        turtle.forward(80-i*2)
+        turtle.left(90)
+        turtle.forward(80-i*2)
+
+
+
+
+    # close to clic
+    turtle.exitonclick()
+
+
+if __name__ == "__main__":
+    GeometryMandala()
